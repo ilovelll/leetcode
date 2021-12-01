132f93e582cad0a138bccbabdcc9cf41957d9ff2
johny.li <gradle@qq.com>
2021-12-01 12:15:27 +0800

    Update solution

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,7 @@
 #[rustfmt::skip]
-pub mod two_sum;                  //   1  easy
-pub mod reverse_integer;          //   7  easy
-pub mod valid_parentheses;        //  20  easy
-pub mod merge_two_sorted_lists;   //  21  easy
-pub mod nth_digit;                // 400  medium
+pub mod two_sum;                  //    1  easy
+pub mod reverse_integer;          //    7  easy
+pub mod valid_parentheses;        //   20  easy
+pub mod merge_two_sorted_lists;   //   21  easy
+pub mod nth_digit;                //  400  medium
+pub mod consecutive_characters;   // 1446  easy
