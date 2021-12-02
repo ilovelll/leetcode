fcaa7559c40f4dc67d0970c8412d770e0a440659
johny.li <gradle@qq.com>
2021-12-02 10:44:14 +0800

    Update solution 506

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,4 +4,5 @@ pub mod reverse_integer;          //    7  easy
 pub mod valid_parentheses;        //   20  easy
 pub mod merge_two_sorted_lists;   //   21  easy
 pub mod nth_digit;                //  400  medium
+pub mod relative_ranks;           //  506  easy
 pub mod consecutive_characters;   // 1446  easy
